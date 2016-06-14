2c0c86f40e76755ed1bdb1812aee7113fcd41cce
Mark DeLillo <mdelillo@pivotal.io>
2016-06-14 16:32:15 -0400

    Make use of concourse\bin\bindata
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -3,4 +3,9 @@ set PATH=%CD%\gopath\bin;%PATH%
 
 go get github.com/jteeuwen/go-bindata
 
+go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
+
+md windows
+.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go windows/...
+
 go build -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
