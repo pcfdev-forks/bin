ca548a09ed74920b254f0cbbe1f1a2dd1e90f706
Mark DeLillo <mdelillo@pivotal.io>
2016-06-14 14:02:40 -0400

    Nothing to package with bindata
    [#120135221]
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,10 +1,4 @@
 set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\Godeps_windows\_workspace
 set PATH=%CD%\gopath\bin;%PATH%
 
-go get github.com/jteeuwen/go-bindata
-
-go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
-
-.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go
-
 go build -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
