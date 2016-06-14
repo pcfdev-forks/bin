838f9b58d29c73b434116cafd2cc7c6b0db86995
Anthony Emengo <aemengo@pivotal.io>
2016-06-14 13:51:42 -0400

    Add support for building windows binary
    [#120135221]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,12 +1,10 @@
 set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\Godeps_windows\_workspace
 set PATH=%CD%\gopath\bin;%PATH%
 
-set /p FinalVersion=<final-version\number
-
 go get github.com/jteeuwen/go-bindata
 
 go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
-.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
+.\go-bindata.exe -pkg bindata -o gopath\src\github.com\concourse\bin\bindata\bindata.go
 
-go build -ldflags "-X github.com/concourse/atc/atccmd.Version=%FinalVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
+go build -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
