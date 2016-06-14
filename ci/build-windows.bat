ac146b654eefd0fc92d499cb7fde074f76d17610
Anthony Emengo <aemengo@pivotal.io>
2016-06-14 14:26:07 -0400

    Still need go-bindata as a dependency
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,4 +1,6 @@
 set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\Godeps_windows\_workspace
 set PATH=%CD%\gopath\bin;%PATH%
 
+go get github.com/jteeuwen/go-bindata
+
 go build -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
