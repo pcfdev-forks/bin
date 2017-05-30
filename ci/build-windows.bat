7ab33287ec9d257e3307b226516b9da19532b973
Nader <nziada@pivotal.io>
2017-05-30 17:10:19 -0400

    remove final-version and fly-rc

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -1,13 +1,12 @@
 set PATH=C:\Go\bin;%PATH%
 
-set GOPATH=%CD%\gopath;%CD%\concourse
+set GOPATH=%CD%\gopath;%CD%\concourse;%CD%\gopath\src\github.com\vito\houdini\deps
 set PATH=%CD%\gopath\bin;%PATH%
 
-set /p FinalVersion=<final-version\version
+set /p FinalVersion=0.0.0
 set /p WorkerVersion=<concourse\src\worker-version\version
 
 mkdir cli-artifacts
-move fly-rc\fly_* cli-artifacts
 
 go get github.com/jteeuwen/go-bindata
 
@@ -15,4 +14,4 @@ go build -o go-bindata.exe github.com/jteeuwen/go-bindata/go-bindata
 
 .\go-bindata.exe -pkg bindata -o concourse\src\github.com\concourse\bin\bindata\bindata.go cli-artifacts/...
 
-go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.WorkerVersion=%WorkerVersion% -X main.WorkerVersion=%WorkerVersion%" -o .\binary\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
+go build -ldflags "-X main.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.Version=%FinalVersion% -X github.com/concourse/atc/atccmd.WorkerVersion=%WorkerVersion% -X main.WorkerVersion=%WorkerVersion%" -o .\binary-windows\concourse_windows_amd64.exe github.com/concourse/bin/cmd/concourse
